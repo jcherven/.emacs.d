7c03318c714b68c0f26327111eddc22f87b3b3d9
jcherven <jamiecherven@gmail.com>
2021-04-26 00:29:19 -0400

    fixes indentation in org and default face

--- init.el
+++ init.el
@@ -55,4 +55,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "SGI" :family "SGI Screen")))))
+ '(default ((t (:height 110 :foundry "SGI" :family "SGI Screen")))))
