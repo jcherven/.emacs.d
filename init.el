f0c24db17bb10e737be4d7dec1b3864d41020624
jcherven <jamiecherven@gmail.com>
2021-04-27 23:28:19 -0400

    configured a default theme via customize

--- init.el
+++ init.el
@@ -26,7 +26,9 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes '(spacemacs-dark))
+ '(beacon-color "green")
+ '(beacon-mode t)
+ '(custom-enabled-themes nil)
  '(custom-safe-themes
    '("3a78cae35163bb71df460ebcfdebf811fd7bc74eaa15428c7e0bccfd4f858d30" "e5dc5b39fecbeeb027c13e8bfbf57a865be6e0ed703ac1ffa96476b62d1fae84" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))
  '(fringe-mode 6 nil (fringe))
@@ -48,11 +50,22 @@
      ("\\?\\?\\?+" . "#dc752f")))
  '(linum-format 'dynamic)
  '(package-selected-packages
-   '(symon dmenu diminish company dashboard rainbow-delimiters sudo-edit hungry-delete evil switch-window avy undo-tree smex ido-vertical-mode beacon spacemacs-theme which-key use-package))
+   '(autothemer symon dmenu diminish company dashboard rainbow-delimiters sudo-edit hungry-delete evil switch-window avy undo-tree smex ido-vertical-mode beacon spacemacs-theme which-key use-package))
  '(pdf-view-midnight-colors '("#655370" . "#fbf8ef")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 110 :foundry "SGI" :family "SGI Screen")))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "SGI" :family "SGI Screen"))))
+ '(cursor ((t (:background "green" :foreground "black"))))
+ '(font-lock-comment-face ((t (:foreground "gray24"))))
+ '(highlight ((t (:background "gray8"))))
+ '(org-level-1 ((t (:inherit outline-1 :weight bold :height 130))))
+ '(org-level-2 ((t (:inherit outline-2 :weight bold :height 130))))
+ '(org-level-3 ((t (:inherit outline-3 :weight bold :height 120))))
+ '(org-level-4 ((t (:inherit outline-4 :weight bold))))
+ '(org-level-5 ((t (:inherit outline-5 :weight bold))))
+ '(org-level-6 ((t (:inherit outline-6 :weight bold))))
+ '(org-level-7 ((t (:inherit outline-7 :weight bold))))
+ '(org-level-8 ((t (:inherit outline-8 :weight bold)))))
